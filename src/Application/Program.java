80d7eb3ba36eac7aef4850dcb7d954c8b6dad300
Frouzin <matheusereis44@gmail.com>
2023-03-11 09:40:57 -0300

    Estoque com POO

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,33 +1,27 @@
 package Application;
 
+import entities.Product;
+
 import java.util.Locale;
 import java.util.Scanner;
 
 public class Program {
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
         Locale.setDefault(Locale.US);
         Scanner scan = new Scanner(System.in);
-        double xA, xB, xC,yA, yB, yC;
 
-        System.out.println("Enter the measures of triangle X: ");
-        xA = scan.nextDouble();
-        xB = scan.nextDouble();
-        xC = scan.nextDouble();
+        Product product = new Product();
 
-        System.out.println("Enter the measures of triangle Y: ");
-        yA = scan.nextDouble();
-        yB = scan.nextDouble();
-        yC = scan.nextDouble();
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        product.name = scan.nextLine();
+        System.out.print("Price: ");
+        product.price = scan.nextDouble();
+        System.out.print("quantatity in stock: ");
+        product.quantity = scan.nextInt();
 
-        double p = (xA+xB+xC)/2.0;
-        double areax = Math.sqrt(p*(p-xA)*(p-xB)*(p-xC));
-
-        p = (yA+yB+yC)/2.0;
-        double areay = Math.sqrt(p*(p-yA)*(p-yB)*(p-yC));
-
-        System.out.printf("Triangle X area: %.4f%n", areax);
-        System.out.printf("Triangle Y area: %.4f%n", areay);
+        System.out.println(product.name + "," + product.price + "," + product.quantity);
 
         scan.close();
     }
